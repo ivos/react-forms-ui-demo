d9c30b8c642be40571e9caf58ea0022f539366f5
Ivo Maixner <ivo.maixner@gmail.com>
2016-09-16 11:38:52 +0200

    Switch to create-react-app.

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import AppRouter from './router'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {useRouterHistory} from 'react-router'
+import {createHashHistory} from 'history'
+import i18n from './i18n'
+import {ReactFormsUiOptions} from 'react-forms-ui'
+import numeral from 'numeral'
+import numeralCs from 'numeral/languages/cs'
+numeral.language('cs', numeralCs)
+import 'react-forms-ui/lib/react-forms-ui.css'
+import './index.css'
+
+ReactFormsUiOptions.translate = i18n.t.bind(i18n)
+
+var supportedLocales = ['en', 'cs']
+var locale = navigator.language.substr(0, 2)
+if (supportedLocales.indexOf(locale) === -1) {
+	locale = 'en'
+}
+
+moment.locale(locale)
+
+const history = useRouterHistory(createHashHistory)({queryKey: false})
+
+numeral.language(locale)
+i18n.changeLanguage(locale, function () {
+	ReactDOM.render(<AppRouter history={history} locale={locale}/>, document.getElementById('app-content'))
+})
